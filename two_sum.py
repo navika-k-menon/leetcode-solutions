981d453e22ade4788a1b896a0bf935c2c96f4a55
Navika <navika.k.menon@gmail.com>
2026-07-03 21:12:11 +0530

    Solved Two Sum

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+# LeetCode 1: Two Sum
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
